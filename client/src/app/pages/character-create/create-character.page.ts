a5f7e978c3ec05d056c320d18035349935ce68cb
seiyria <kyle@seiyria.com>
2020-12-27 20:58:56 -0600

    set up char create

--- client/src/app/pages/character-create/create-character.page.ts
+++ client/src/app/pages/character-create/create-character.page.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+enum CharacterCreateStep {
+  CampaignOrNo = 'campaigncode',
+  Archetype = 'archetype',
+  NameSpecies = 'namespecies',
+  Background = 'background',
+  Drives = 'drives',
+  Natures = 'natures',
+  Connections = 'connections',
+  BonusStats = 'bonusstats',
+  Moves = 'moves',
+  Finalize = 'finalize'
+}
+
 @Component({
   selector: 'app-create-character',
   templateUrl: './create-character.page.html',
   styleUrls: ['./create-character.page.scss'],
 })
 export class CreateCharacterPage implements OnInit {
+
+  public readonly Step = CharacterCreateStep;
+  public currentStep: CharacterCreateStep;
 
   constructor() { }
 
