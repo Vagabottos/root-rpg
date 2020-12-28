5a5569c5e673db37a32ab16a04a7fcc4aa9da821
seiyria <kyle@seiyria.com>
2020-12-27 19:44:46 -0600

    add character frontend

--- client/src/app/pages/dashboard-characters/dashboard-characters.page.ts
+++ client/src/app/pages/dashboard-characters/dashboard-characters.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ICharacter } from '../../models';
+import { CharacterAPIService } from '../../services/character.api.service';
 
 @Component({
   selector: 'app-dashboard-characters',
@@ -10,9 +11,35 @@ export class DashboardCharactersPage implements OnInit {
 
   public characters: ICharacter[] = [];
 
-  constructor() { }
+  private page = 0;
+
+  constructor(private characterAPI: CharacterAPIService) { }
 
   ngOnInit() {
+    this.loadCharacters(0);
+  }
+
+  loadCharacters(page = 0, $event = null): void {
+    this.characterAPI.getCharacters(page)
+      .subscribe(characters => {
+        if ($event) {
+          this.characters.push(...characters.data);
+
+          if (this.characters.length >= characters.total) {
+            $event.target.disabled = true;
+          }
+
+          $event.target.complete();
+          return;
+        }
+
+        this.characters = characters.data;
+      });
+  }
+
+  loadMoreCharacters($event) {
+    this.page += 1;
+    this.loadCharacters(this.page, $event);
   }
 
 }
--- client/src/app/services/character.api.service.ts
+++ client/src/app/services/character.api.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICharacter, ITableData } from '../models';
+import { APIService } from './api.service';
+import { UserAPIService } from './user.api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterAPIService {
 
-  constructor() { }
+  constructor(
+    private api: APIService,
+    private userAPI: UserAPIService,
+    private http: HttpClient
+  ) { }
+
+  createCharacter(opts: ICharacter): Observable<ICharacter> {
+    return this.http.post(this.api.apiUrl('/character'), opts) as Observable<ICharacter>;
+  }
+
+  getCharacters(page = 0): Observable<ITableData<ICharacter>> {
+    const limit = 10;
+
+    const params = new HttpParams()
+      .set('owner', this.userAPI.userId)
+      .set('$sort[updatedAt]', '-1')
+      .set('$skip', (page * limit).toString())
+      .set('$limit',  limit.toString());
+
+    return this.http.get(this.api.apiUrl(`/character`), { params }) as Observable<ITableData<ICharacter>>;
+  }
+
+  loadCharacter(id: string): Observable<ICharacter> {
+    return this.http.get(this.api.apiUrl(`/character/${id}`)) as Observable<ICharacter>;
+  }
 }
