8b6666a823a44c90f6a446d24e887b200379c092
seiyria <kyle@seiyria.com>
2021-01-03 21:47:31 -0600

    slightly more stable longpress

--- client/src/app/directives/longpress.directive.ts
+++ client/src/app/directives/longpress.directive.ts
@@ -1,86 +1,63 @@
-import { Directive, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
-import { combineLatest, interval, Observable, Subject } from 'rxjs';
-import { filter, first, map, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, fromEvent, interval, merge, Observable, of, race, Subject, timer } from 'rxjs';
+import { filter, first, flatMap, map, mergeMap, startWith, switchMap, switchMapTo, takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[appLongPress]'
 })
 export class LongPressDirective implements OnInit, OnDestroy {
 
-    @Input() public longPress = 500;
+    @Input() public longPress = 1000;
     @Output() public release: EventEmitter<MouseEvent> = new EventEmitter();
 
-    public mouseups$ = new Subject();
-    public mousedowns$ = new Subject();
-    public destroys$ = new Subject();
+    public destroy$ = new Subject();
+
+    public touchStartSubject: Subject<any> = new Subject<any>();
+    public touchStartObservable: Observable<any> = this.touchStartSubject.asObservable();
+
+    public touchEndSubject: Subject<any> = new Subject<any>();
+    public touchEndObservable: Observable<any> = this.touchEndSubject.asObservable();
 
     constructor() {}
 
     ngOnInit(): void {
-    }
-
-    private initClick(event: MouseEvent): void {
-      const interval$ = this.interval$()
+      this.touchStartObservable
         .pipe(
-          takeUntil(this.mouseups$)
-        );
-
-      combineLatest([this.mousedowns$, this.mouseups$, interval$])
-        .pipe(first())
-        .subscribe((val) => {
-          const endEvent = val[0] as MouseEvent;
-          const startEvent = val[1] as MouseEvent;
-          
-          const dist = Math.sqrt(
-            Math.pow(endEvent.x - startEvent.x, 2) + Math.pow(endEvent.y - startEvent.y, 2)
-          );
-
-          if(dist > 50) return;
-
-          this.release.emit(endEvent);
+          takeUntil(this.destroy$),
+          mergeMap((res) => race(
+            timer(this.longPress).pipe(map(() => res)),
+            this.touchEndObservable,
+          )),
+        )
+        .subscribe((res: TouchEvent) => {
+          if (!res) return;
+          this.release.emit();
         });
     }
 
-    public ngOnDestroy(): void {
-      this.destroys$.next();
-      this.destroys$.unsubscribe();
-    }
-
-    public interval$(): Observable<number> {
-      return interval()
-        .pipe(
-          map(i => i * 10),
-          filter(i => i >= this.longPress)
-        );
-    }
-
-    @HostListener('contextmenu', ['$event'])
-    onCtx(event): boolean {
-      event.preventDefault();
-      event.stopPropagation();
-      return false;
+    ngOnDestroy() {
+      this.destroy$.next();
+      this.destroy$.unsubscribe();
     }
 
     @HostListener('touchstart', ['$event'])
-    public onTouchStart(event: MouseEvent): void {
-      this.initClick(event);
-      this.mousedowns$.next(event);
-    }
-
-    @HostListener('touchend', ['$event'])
-    public onTouchEnd(event: MouseEvent): void {
-      this.mouseups$.next(event);
+    public touchStart($event: TouchEvent): void {
+      this.touchStartSubject.next($event);
     }
 
     @HostListener('mousedown', ['$event'])
-    public onMouseDown(event: MouseEvent): void {
-      this.initClick(event);
-      this.mousedowns$.next(event);
+    public mouseDown($event: TouchEvent): void {
+      this.touchStartSubject.next($event);
+    }
+
+    @HostListener('touchend', ['$event'])
+    public touchEnd(): void {
+      this.touchEndSubject.next(null);
     }
 
     @HostListener('mouseup', ['$event'])
-    public onMouseUp(event: MouseEvent): void {
-      this.mouseups$.next(event);
+    public mouseUp(): void {
+      this.touchEndSubject.next(null);
     }
 }
