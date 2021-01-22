987726bceb239ab2631186538fa01017705d436b
seiyria <kyle@seiyria.com>
2021-01-22 04:17:05 -0600

    handling jwt expired more gracefully

--- client/src/app/interceptors/error.interceptor.ts
+++ client/src/app/interceptors/error.interceptor.ts
@@ -5,19 +5,29 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { NotificationService } from '../services/notification.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
   private readonly badMessages = ['No record found for id'];
 
-  constructor(private notification: NotificationService) {}
+  constructor(
+    private router: Router,
+    private notification: NotificationService
+  ) {}
 
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     return next.handle(httpRequest)
       .pipe(catchError((error: HttpErrorResponse) => {
         const errorMsg = error?.error?.message ?? 'Unknown error.';
+
+        if(error?.error.code === 401) {
+          this.router.navigate(['/login']);
+          this.notification.notify('Your previous session has expired. Redirecting to login.');
+          return;
+        }
 
         // some messages are pretty annoying and shouldnt be shown
         if (!this.badMessages.every(msg => errorMsg.includes(msg))) {
