a3cefa67696d214ab5bd75edb718b91a6f58eb25
Kyle Kemp <kyle@seiyria.com>
2021-01-23 17:02:33 -0600

    add confirm to character/campaign back buttons

--- client/src/app/pages/dashboard/dashboard-routing.module.ts
+++ client/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../../guards/auth.guard';
+import { UnsavedChangesGuard } from '../../guards/confirm-leave.guard';
 
 import { DashboardPage } from './dashboard.page';
 
@@ -20,12 +21,14 @@ const routes: Routes = [
       {
         path: 'campaigns/create',
         loadChildren: () => import('../campaign-create/create-campaign.module').then( m => m.CreateCampaignPageModule),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        canDeactivate: [UnsavedChangesGuard]
       },
       {
         path: 'characters/create',
         loadChildren: () => import('../character-create/create-character.module').then( m => m.CreateCharacterPageModule),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        canDeactivate: [UnsavedChangesGuard]
       },
       {
         path: '**',
