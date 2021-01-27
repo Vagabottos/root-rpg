3b1e46874b461eea391098068748812cdbfd7eef
Kyle Kemp <kyle@seiyria.com>
2021-01-27 17:47:22 -0600

    fix harm migration for npcs

--- server/src/helpers/clean-npc.ts
+++ server/src/helpers/clean-npc.ts
@@ -10,6 +10,9 @@ export function cleanNPC(npc: INPC): void {
   npc.drive = clean(npc.drive);
   npc.notes = clean(npc.notes, 1000);
 
+  npc.harmMax = npc.harmMax || { injury: 1, exhaustion: 1, depletion: 1, morale: 1 };
+  npc.harm = npc.harm || { injury: 0, exhaustion: 0, depletion: 0, morale: 0 };
+
   ['injury', 'exhaustion', 'depletion', 'morale'].forEach(harm => {
     npc.harmMax[harm] = Math.floor(Math.max(0, Math.min(5, npc.harmMax[harm])));
     npc.harm[harm] = Math.floor(Math.max(0, Math.min(5, npc.harm[harm])));
