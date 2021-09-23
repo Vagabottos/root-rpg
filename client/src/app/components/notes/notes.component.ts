c14c712524fa3a0606e4aecb9f0a5cc9270a8e04
Kyle Kemp <kyle@seiyria.com>
2021-09-23 10:47:44 -0500

    better focus on notes

--- client/src/app/components/notes/notes.component.ts
+++ client/src/app/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { IonTextarea } from '@ionic/angular';
 
 @Component({
   selector: 'app-notes',
@@ -11,6 +12,8 @@ export class NotesComponent implements OnInit {
   @Input() notes: string;
   @Output() updateNotes: EventEmitter<string> = new EventEmitter();
 
+  @ViewChild(IonTextarea) notesElement: IonTextarea;
+
   public isEditing: boolean;
 
   constructor(
@@ -21,6 +24,10 @@ export class NotesComponent implements OnInit {
 
   toggleEditing() {
     this.isEditing = !this.isEditing;
+
+    if (this.isEditing) {
+      this.notesElement.setFocus();
+    }
 
     if (!this.isEditing) {
       this.updateNotes.emit(this.notes);
