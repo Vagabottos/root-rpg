a45c873ff6d020c2bffe43247ef0001a549f3693
Kyle Kemp <kyle@seiyria.com>
2021-10-14 10:22:48 -0500

    start change password page

--- client/src/app/pages/dashboard-account/dashboard-account.page.ts
+++ client/src/app/pages/dashboard-account/dashboard-account.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { UserAPIService } from '../../services/user.api.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-dashboard-account',
@@ -10,10 +12,19 @@ import { UserAPIService } from '../../services/user.api.service';
 })
 export class DashboardAccountPage implements OnInit {
 
+  public currentPassword = '';
+  public newPassword = '';
+
+  public passwordForm = new FormGroup({
+    currentPassword: new FormControl('', [Validators.required]),
+    newPassword: new FormControl('', [Validators.required])
+  });
+
   constructor(
     private alert: AlertController,
     private router: Router,
-    public api: UserAPIService
+    public api: UserAPIService,
+    private notify: NotificationService
   ) { }
 
   ngOnInit() {
@@ -30,6 +41,27 @@ export class DashboardAccountPage implements OnInit {
           handler: () => {
             this.api.logout();
             this.router.navigate(['/login']);
+
+            this.notify.notify('You have been successfully logged out!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  async changePassword() {
+    const alert = await this.alert.create({
+      header: 'Change Password',
+      message: `Are you sure you want to change your password?`,
+      buttons: [
+        'Cancel',
+        {
+          text: 'Yes, change password',
+          handler: () => {
+            console.log(this.passwordForm.get('currentPassword').value, this.passwordForm.get('newPassword').value);
+            this.notify.notify('Your password has been changed!');
           }
         }
       ]
