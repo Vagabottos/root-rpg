524c45c9354c23514d359599fd624a9ac01096a0
Kyle Kemp <kyle@seiyria.com>
2023-02-26 09:21:46 -0600

    attempt to use specific mandrill library

--- server/src/services/mailer/mailer.service.ts
+++ server/src/services/mailer/mailer.service.ts
@@ -1,6 +1,7 @@
 // Initializes the `mailer` service on path `/services/mailer`
 import { ServiceAddons } from '@feathersjs/feathers';
 import Mailer from 'feathers-mailer';
+import mandrill from 'nodemailer-mandrill-transport';
 import { Application } from '../../declarations';
 import hooks from './mailer.hooks';
 
@@ -14,7 +15,12 @@ declare module '../../declarations' {
 export default async function (app: Application): Promise<void> {
 
   // Initialize our service with any options it requires
-  app.use('/mailer', Mailer(app.get('mail')));
+  const defaultMailSettings = app.get('mail');
+  app.use('/mailer', Mailer(mandrill({
+    auth: {
+      apiKey: defaultMailSettings.auth.pass
+    }
+  })));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('mailer');
