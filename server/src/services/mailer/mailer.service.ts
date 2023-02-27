2f63d7a8e5bea890c2733e7ea24ec6ce04d2e315
Kyle Kemp <kyle@seiyria.com>
2023-02-27 15:12:04 -0600

    update mailer

--- server/src/services/mailer/mailer.service.ts
+++ server/src/services/mailer/mailer.service.ts
@@ -1,7 +1,6 @@
 // Initializes the `mailer` service on path `/services/mailer`
 import { ServiceAddons } from '@feathersjs/feathers';
 import Mailer from 'feathers-mailer';
-import nodemailer from 'nodemailer';
 import { Application } from '../../declarations';
 import hooks from './mailer.hooks';
 
@@ -16,17 +15,8 @@ export default async function (app: Application): Promise<void> {
 
   // Initialize our service with any options it requires
   const defaultMailSettings = app.get('mail');
-  const transport = nodemailer.createTransport({
-    service: 'Mandrill',
-    auth: {
-      user: defaultMailSettings.auth.user,
-      pass: defaultMailSettings.auth.pass
-    },
-    logger: true,
-    debug: true
-  });
 
-  app.use('/mailer', Mailer(transport, defaultMailSettings));
+  app.use('mailer', Mailer(defaultMailSettings));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('mailer');
